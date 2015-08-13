e982c0a59c7f54483349faeb04ff9400503967e7
Nick Luo <luotianqi@dihmed.com>
2015-08-13 09:04:37 +0800

    Add default voices

---
+++ Voices/FSBuild.bat
@@ -0,0 +1,8 @@
+.\Flashfs Format
+.\Flashfs Write 1-ScanPresId.mp3
+.\Flashfs Write 2-Get.mp3
+.\Flashfs Write 3-Position.mp3
+.\Flashfs Write 4-Timeout.mp3
+.\Flashfs Write 5-Rep.mp3
+.\Flashfs Write 6-None.mp3
+.\Flashfs Info
